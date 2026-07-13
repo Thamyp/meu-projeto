96195997f7f577cb19883421e4a703fbbbb1e28b
Thamy Marcondes <thamylucena23@gmail.com>
2026-07-13 07:18:17 -0300

    Atualizações

--- Fundamentos/imc.py
+++ Fundamentos/imc.py
@@ -2,4 +2,4 @@ peso = int(input("Digite seu peso em quilos: "))
 altura = int(input("Qual a sua altura? "))
              
 print("você é pesado, ", peso, "Esse é seu peso")
-
+print("Ops, você também é alto")
